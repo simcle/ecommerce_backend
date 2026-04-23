5a89e8901c9dba5d365fb556224c10f2465043ea
NendiSofiandy <nendisofiandy80@gmail.com>
2026-04-23 23:03:49 +0700

    update

--- src/cron/analytics.cron.js
+++ src/cron/analytics.cron.js
@@ -1,21 +1,27 @@
 import cron from "node-cron"
 import { syncTodayAnalyticsHourly } from "../services/analytics.service.js"
 
-export const startAnalyticsCron = () => {
-  cron.schedule(
-  "5 * * * *",
-  async () => {
-    console.log("[CRON] analytics hourly started")
+export const startAnalyticsCron = async() => {
+    cron.schedule(
+    "5 * * * *",
+    async () => {
+      console.log("[CRON] analytics hourly started")
 
-    try {
-      await syncTodayAnalyticsHourly()
-      console.log("[CRON] analytics hourly success")
-    } catch (error) {
-      console.error("[CRON] analytics hourly error:", error.message)
+      try {
+        await syncTodayAnalyticsHourly()
+        console.log("[CRON] analytics hourly success")
+      } catch (error) {
+        console.error("[CRON] analytics hourly error:", error.message)
+      }
+    },
+    {
+      timezone: "Asia/Jakarta"
     }
-  },
-  {
-    timezone: "Asia/Jakarta"
-  }
-)
+  )
+  try {
+        await syncTodayAnalyticsHourly()
+        console.log("[CRON] analytics hourly success")
+      } catch (error) {
+        console.error("[CRON] analytics hourly error:", error.message)
+      }
 }
